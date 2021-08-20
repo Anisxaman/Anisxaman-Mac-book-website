53e9e221eca92adbb3dafe66c4c59330950e4a6b
Anisxaman <83417019+Anisxaman@users.noreply.github.com>
2021-08-21 04:04:41 +0600

    all done without clean comment

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,8 @@
-function memory(num1,idpara){
+
+// -----------------------(Function for counting price)-------------------------------------------
+
+function 
+memory(num1,idpara){
 
 
     const memoryCost=document.getElementById(idpara);
@@ -11,42 +15,46 @@ function memory(num1,idpara){
 
 
 // ------------------------------------------calculation ------------------------------------------
-function calculation(){
+function 
+calculation(){
 //    const BestPrice= document.getElementById("BestPrice");
 //    const BestPhnPrice=eval(BestPrice.innerText);
-   const memoryCost=document.getElementById("memoryCost").innerText;
-   const extraMemoryCost=eval(memoryCost);
-   const storageCost=document.getElementById("storageCost").innerText;
-   const extraStorageCost=eval(storageCost);
-   const deliveryCost=document.getElementById("deliveryCharge").innerText;
-   const deliveryCharge=eval(deliveryCost);
-    const TotalPrice=document.getElementById("totalPrice");
-    const TotalAmountOfPrice=eval(TotalPrice.innerText);
+        const memoryCost=document.getElementById("memoryCost").innerText;
+        const extraMemoryCost=eval(memoryCost);
+        const storageCost=document.getElementById("storageCost").innerText;
+        const extraStorageCost=eval(storageCost);
+        const deliveryCost=document.getElementById("deliveryCharge").innerText;
+        const deliveryCharge=eval(deliveryCost);
+        const TotalPrice=document.getElementById("totalPrice");
+        const TotalAmountOfPrice=eval(TotalPrice.innerText);
 
-    console.log(TotalAmountOfPrice);
-    console.log(extraStorageCost);
-    console.log(deliveryCharge);
+        console.log(TotalAmountOfPrice);
+        console.log(extraStorageCost);
+        console.log(deliveryCharge);
 
-TotalPrice.innerText=extraMemoryCost+extraStorageCost+deliveryCharge +1299;
-const allTotalPrice=TotalPrice.innerText;
+        TotalPrice.innerText=extraMemoryCost+extraStorageCost+deliveryCharge +1299;
+        const allTotalPrice=TotalPrice.innerText;
 
-discount(allTotalPrice)
+        discount(allTotalPrice);
+        }
+
+// --------------------------------(Total price Display)----------------------------------------
+
+function 
+discount(price){
+            const discountTotal=  document.getElementById("finalTotal");
+            discountTotal.innerText=price;
+
 }
 
 
 
-function discount(price){
-  const discountTotal=  document.getElementById("finalTotal");
-  discountTotal.innerText=price;
-
-}
 
 
 
+//------------------------------------------(All event)-------------------------------------------
 
 
-
-//------------------------------------------ event---------------------------------------------------
 document.getElementById("memoryBtnOne").addEventListener('click',function(){
     memory(0,"memoryCost");
     
@@ -78,7 +86,7 @@ document.getElementById("deliveryBtnTwo").addEventListener('click',function(){
 
 
 
-// ------------------------------------------------------
+// ------------------------------(Cupon Code Section)-------------------------------------
 
 
 
@@ -92,28 +100,58 @@ document.getElementById('input').addEventListener('keyup',function(event){
     else{
         applyBtn.setAttribute('disabled',true);
     }
-
- 
-    deteteField.value=''
+          
  })
 
 
-document.getElementById("applyBtn").addEventListener('click',function(){
+// document.getElementById("applyBtn").addEventListener('click',function(){
     
-    const input=document.getElementById("input");
-    // const inputfield=input.value;
-    const FinalAmount= document.getElementById('finalTotal');
-    const discountFinalAmount=eval( FinalAmount.innerText);
+//     const input=document.getElementById("input");
+//     // const inputfield=input.value;
+//     const FinalAmount= document.getElementById('finalTotal');
+//     const discountFinalAmount=eval( FinalAmount.innerText);
     
    
-    FinalAmount.innerText= discountFinalAmount*0.8;
-    input.value='';
+//     FinalAmount.innerText= discountFinalAmount*0.8;
+//     input.value='';
+
+// })
+
+
+// ----------------------------(function for cupon code try exceed)--------------------------------------
+    var clicks = 0;
+
+    function 
+    onClick() {
+      clicks += 1;
+     return clicks;
+    };
+
+
+
+
+// -----------------------------------------------
+document.getElementById("applyBtn").addEventListener('click',function(){
+              const result=onClick();
+        if(result==1){
+                const input=document.getElementById("input");
+                const FinalAmount= document.getElementById('finalTotal');
+                const discountFinalAmount=eval( FinalAmount.innerText);
+                
+            
+                FinalAmount.innerText= discountFinalAmount*0.8;
+                input.value='';
+
+                    }
+        else{
+          alert("You can apply only one time promo code!!!");
+        }
+      
 
 })
 
 
 
-// -----------------------------------------------
 
 
 
